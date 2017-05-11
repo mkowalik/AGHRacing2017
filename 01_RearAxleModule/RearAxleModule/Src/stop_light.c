fb7810d07b7b1e22cda85cc544caa0e3fa850822
Wojciech Gredel <wojciechgredel@gmail.com>
2017-05-11 23:53:20 +0200

    RearAxleModule, stoplight, new configuration of project

---
+++ 01_RearAxleModule/RearAxleModule/Src/stop_light.c
@@ -0,0 +1,40 @@
+/*
+ * stop_light.c
+ *
+ *  Created on: May 11, 2017
+ *      Author: Wojciech
+ */
+
+#include "stop_light.h"
+#include "main.h"
+#include "gpio.h"
+
+typedef enum StopLight_value{
+	OFF = 0,
+	ON
+}StopLight_value_t;
+
+static void StopLight_Write(StopLight_value_t value){
+
+	switch(value){
+
+	case OFF:
+		HAL_GPIO_WritePin(STOP_LIGHT_GPIO_Port, STOP_LIGHT_Pin, GPIO_PIN_RESET);
+		break;
+
+	case ON:
+		HAL_GPIO_WritePin(STOP_LIGHT_GPIO_Port, STOP_LIGHT_Pin, GPIO_PIN_SET);
+		break;
+
+	}
+
+}
+
+void StopLight_Set(void){
+	StopLight_Write(OFF);
+}
+
+void StopLight_Clr(void){
+	StopLight_Write(ON);
+}
+
--- 01_RearAxleModule/RearAxleModule/Src/tim.c
+++ 01_RearAxleModule/RearAxleModule/Src/tim.c
@@ -231,6 +231,7 @@ void HAL_TIM_PWM_MspDeInit(TIM_HandleTypeDef* tim_pwmHandle)
 
 /* USER CODE BEGIN 1 */
 
+
 /* USER CODE END 1 */
 
 /**
