549dd62f3136ecc6df5a8dd607d4788a8f068453
Mondiegus <Mondiegus9@gmail.com>
2017-05-18 15:23:56 +0200

    GPS v1
    Po uruchomieniu podpięciu zasilania wysyłane są informacje o rozruchu
    układu SIM28(COLD START), oraz polecenie akwizycji tylko i wyłącznie
    danych dot. położenia.
    Do odczytu danych zrobiony jest bufor, który może zgromadzić 35 znaków
    ale nie jestem tego pewny a w dokumentacji nie jest powiedziane jak
    obszerne informacje wysyła moduł. Dodałem baaaardzo podstawową obsługę
    wysyłania danych do karty SD, ale jest to w zasadzie sprawdzenie czy
    karta SD jest wpięta i ewentualne wysłanie odebranych danych. Nic
    wielkiego ale uwzględniłem...
    Komunikację, czyli wysyłanie i odbieranie danych po UART'cie sprawdziłem
    na swoim Nucleo i to działa(ło) . Co do reszty, to nie wiem jak te
    komendy zadziałają na SIM28 bo znalazłem 3 różne pdf'y z nimi i nieco
    się różniły (wziąłem nanowsze wydanie).
    Komunikacja więc na pewno działa ale nie wiem jaka będzie reakcja
    układu.

--- 22_GpsAndDataloggerBox/GpsAndDataloggerBox/Src/main.c
+++ 22_GpsAndDataloggerBox/GpsAndDataloggerBox/Src/main.c
@@ -47,6 +47,9 @@
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
+uint8_t Received[35]; // Tablica przechowywujaca odebrane dane
+uint8_t Data[40]; // Tablica przechowujaca wysylana wiadomosc
+uint16_t size = 0; // Rozmiar wysylanej wiadomosci
 
 /* USER CODE END PV */
 
@@ -56,7 +59,17 @@ void Error_Handler(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+ size = sprintf(Data, "%s\n\r",Received);//Odebrana wiadomosc
 
+ //HAL_UART_Transmit_IT(&huart1, Data, size); // Rozpoczecie nadawania danych z wykorzystaniem przerwan
+ HAL_UART_Receive_IT(&huart1, Received, 35); // Ponowne wlaczenie nasluchiwania i zapis do Received
+ //Write data to SD CARD
+ if(HAL_SD_GetStatus(*hsd))
+ {
+	 HAL_SD_WriteBlocks(*hsd,*Received,12,512,1);
+ }
+}
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
@@ -86,7 +99,12 @@ int main(void)
   MX_RTC_Init();
 
   /* USER CODE BEGIN 2 */
-
+  // Na pewno brakuje weryfikacji poprawnego uruchomienia, ale do tego potrzebny mi jest modul
+  size = sprintf(data, "$PMTK104*37<CR><LF>", cnt); // FULL COLD START
+  HAL_UART_Transmit(&huart1, data, size,1); // Rozpoczecie nadawania danych z wykorzystaniem przerwan
+  size = sprintf(data, "$PMTK314,0,1,0,0,0,,0,0,0,0,0,0,0,0,0,0,0,1,0*2Ｄ<CR><LF>", cnt); // Odbior tylko danych dot. polozenia
+  HAL_UART_Transmit(&huart1, data, size,1); // Rozpoczecie nadawania danych z wykorzystaniem przerwan
+  HAL_UART_Receive_IT(&huart1, Received, 45);
   /* USER CODE END 2 */
 
   /* Infinite loop */
